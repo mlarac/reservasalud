610952f61b7c3eb640a271d65aa8bbf297b715da
Marcelo Lara <mlarac7@gmail.com>
2025-02-15 23:01:46 -0300

    se crean usuarios para pruebas

---
+++ public/user.js
@@ -0,0 +1,5 @@
+[
+    { "id": 1, "email": "usuario1@example.com", "password": "123456", "name": "Usuario Uno" },
+    { "id": 2, "email": "usuario2@example.com", "password": "abcdef", "name": "Usuario Dos" }
+  ]
+  
