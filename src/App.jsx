4002b62f9eb0f7e4b1db4a21f76a1d6f3ea89d8f
Alan Barrueto <barruetoalan9@gmail.com>
2025-02-16 22:15:52 -0300

    Se agregan las rutas de reservar cita, configuración profesional y gestion disponibilidad

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import LoginView from './views/LoginView';
 import RegisterView from './views/RegisterView';
+import ConfiguracionProfesional from './views/ConfiguracionProfesional';
+import ReservarCita from './views/ReservarCita';
+import GestionDisponibilidad from './views/GestionDisponibilidad';
 // ... otras importaciones
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
           {/* ... otras rutas */}
           <Route path="/login" element={<LoginView />} />
           <Route path="/register" element={<RegisterView />} />
+          <Route path="/config" element={<ConfiguracionProfesional />} />
+          <Route path="/reservar-cita" element={<ReservarCita />} />
+          <Route path="/gestion-disponibilidad" element={<GestionDisponibilidad />} />
         </Routes>
       </Router>
     </AuthProvider>
