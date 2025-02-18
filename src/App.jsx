2b1f45e8c3a672eb42fafd24ef632c20cf77798f
Alan Barrueto <barruetoalan9@gmail.com>
2025-02-18 20:52:28 -0300

    se agregan imágenes y se modifican los css para que funcione con el proyecto general. También se hacen alguna modificaciones a algunos archivos css que ya existían para que funcione con el nuevo estilo

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,45 @@ import { AuthProvider } from './context/AuthContext';
 import LoginView from './views/LoginView';
 import RegisterView from './views/RegisterView';
 import ConfiguracionProfesional from './views/ConfiguracionProfesional';
-import ReservarCita from './views/ReservarCita';
 import GestionDisponibilidad from './views/GestionDisponibilidad';
-// ... otras importaciones
+import ReservarCita from './views/ReservarCita';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import HomePage from './views/Home';
+import Nosotros from './views/Nosotros';
+import IntranetPage from './views/IntranetPage'; // <-- Importa la nueva vista
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './App.css';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          {/* ... otras rutas */}
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/register" element={<RegisterView />} />
-          <Route path="/config" element={<ConfiguracionProfesional />} />
-          <Route path="/reservar-cita" element={<ReservarCita />} />
-          <Route path="/gestion-disponibilidad" element={<GestionDisponibilidad />} />
-        </Routes>
+        <div className="d-flex flex-column min-vh-100">
+          <Navbar /> {/* Navbar fijo */}
+
+          {/* Contenedor principal con padding para evitar solapamiento */}
+          <main className="flex-grow-1 content-wrapper">
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path="/login" element={<LoginView />} />
+              <Route path="/register" element={<RegisterView />} />
+
+              {/* Nueva ruta Intranet */}
+              <Route path="/intranet" element={<IntranetPage />} />
+
+              {/* Rutas ya existentes */}
+              <Route path="/config" element={<ConfiguracionProfesional />} />
+              <Route path="/gestion-disponibilidad" element={<GestionDisponibilidad />} />
+              <Route path="/reservar-cita" element={<ReservarCita />} />
+              <Route path='/nosotros' element={<Nosotros />} />
+            </Routes>
+          </main>
+
+          <Footer /> {/* Footer */}
+        </div>
       </Router>
     </AuthProvider>
   );
